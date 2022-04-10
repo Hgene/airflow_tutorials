e701acf1f59ca152f59f67b6d2f10387d518648d
hyungjin <47958965+Hgene@users.noreply.github.com>
2022-04-11 00:19:54 +0900

    Update hello_world_dag.py

--- env_setting/hello_world_dag.py
+++ env_setting/hello_world_dag.py
@@ -6,16 +6,30 @@ from datetime import datetime
 
 def helloWorld():
     print('Hello World')
+    return 0
+    
+def helloWorld_v2():
+    print('Hello World V2')
     return 1
-
+    
+    
+#(1) DAG를 정의
 with DAG(dag_id="hello_world_dag", 
         start_date=datetime(2021,1,1), 
         schedule_interval="15 * * * *", 
         catchup=False) as dag:
-
+        
+    #(2) task를 정의
     task1 = PythonOperator(
         task_id="hello_world", 
         python_callable=helloWorld
+        
+    task2 = PythonOperator(
+        task_id="hello_world_v2", 
+        python_callable=helloWorld_v2
     )
 
-task1
+#(3)실행순서에 맞추어 Pipeline 구성
+task1 >> task2
+        
+        
